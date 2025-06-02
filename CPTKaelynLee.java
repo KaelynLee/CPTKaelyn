1a8c0e6cae7d8a71b65e25b2c9f86717e321695d
KaelynLee <kaelyn.lee27@ycdsbk12.ca>
2025-06-02 09:41:45 -0400

    Update CPTKaelynLee.java

--- CPTKaelynLee.java
+++ CPTKaelynLee.java
@@ -4,7 +4,34 @@ public class CPTKaelynLee{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+		String strName;
+		String strType;
+		int intCount=0
 		
+		con.println("Give me your name");
+		strName = con.readLine();
+		
+		con.println("Play Game");
+		
+		con.println("View Leaderboard");
+		
+		con.println("Add Quiz");
+		
+		con.println("Quit");
+		
+		
+		TextInputFile masterfile = new TextInputFile("quizzes.txt");
+		while(masterfile.eof() == false){
+			strType = masterfile.readLine();
+			intCount++;
+			con.println(strType);
+		
+		if(intCount = 1){
+			con.println
+			
+			
+			}
+		}
 		
 	}
 }
