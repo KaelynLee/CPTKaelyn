a2600468f022c6e51a0dbab365ed97e5cc57e876
KaelynLee <kaelyn.lee27@ycdsbk12.ca>
2025-06-03 23:05:43 -0400

    Added a image logo, added a font with game title, attempted to include an 2D array for quizzes

--- CPTKaelynLee.java
+++ CPTKaelynLee.java
@@ -8,24 +8,27 @@ public class CPTKaelynLee{
 		Console con = new Console(700,700);
 		
 		String strName;
+		String strQuizName[][];
 		String strType;
 		int intCount=0;
-		int intQuizCount = 0;
-		int intQuestionsCount;
 		int intChoice;
 		
 		
 		con.setDrawColor(Color.WHITE);
 		con.fillRect(0,0,1366,768);
 		con.setTextColor(Color.BLACK); 
-		Font fntTest = con.loadFont("RushfordPrintedDemoRegular.ttf", 600);
+		Font fntTest = con.loadFont("RushfordPrinted-MVnDe.ttf", 65);
 		con.setDrawFont(fntTest);
 		con.setDrawColor(new Color(0, 0, 0));
-		con.drawString("Math Training Game",250,80);
+		con.drawString("Math Training Game",110,30);
+		int intX = 100;
+		int intY = 100;
+		BufferedImage imgMath = con.loadImage("mathlogo.jpeg");
+		con.drawImage(imgMath,intX,intY);
+		
 		
 			
-		con.println("Give me your name");
-		strName = con.readLine();
+		
 		
 		con.println("(1)Play Game");
 		
@@ -38,12 +41,34 @@ public class CPTKaelynLee{
 		intChoice = con.readInt();
 		
 		if(intChoice == 1){
+			con.println("Give me your name");
+			strName = con.readLine();
 			TextInputFile masterfile = new TextInputFile("quizzes.txt");
+			
 			while(masterfile.eof() == false){
 				strType = masterfile.readLine();
+				//the number of quizzes avaliable to choose from
 				intCount++;
 				con.println(intCount + ") " + strType);		
 	
+		}
+		
+		
+		con.println("Choose a quiz by number");
+		intChoice = con.readInt();
+		
+		if(intChoice>0 && intChoice<= intCount){
+			
+			
+			strQuizName = new String[100][5];
+			strQuizName[0][0] = ("What is 2+2");
+			strQuizName[0][1] = ("4");
+			strQuizName[0][2] = ("Four");
+			strQuizName[0][3] = ("+4");
+			
+			con.println(strQuizName[0][0]);
+			 con.println("A) " + strQuizName[0][1]);
+			
 		}
 		
 		}else if(intChoice == 2){
@@ -57,14 +82,6 @@ public class CPTKaelynLee{
 		
 		}
 		
-		con.println("Choose a quiz by number");
-		intChoice = con.readInt();
-		
-		if(intQuizCount == intChoice){
-			String[][] strQuiz = new String[100][5];
-			
-			
-		}
 		
 		
 		
