e121182a42e6386a998bb225d4713fcb7adef851
KaelynLee <kaelyn.lee27@ycdsbk12.ca>
2025-06-08 10:27:47 -0400

    Edited the main game screen for all required information to appear correctly. Used two commands con.repaint for code to function correctly. Added a score counter to calculate game score and ending percentage. Edited the enter name section using con.drawString.

--- CPTKaelynLee.java
+++ CPTKaelynLee.java
@@ -32,49 +32,59 @@ public class CPTKaelynLee{
 	
 		Font fntOption=con.loadFont("NewminRegular-ov6aa.ttf",45);
 		con.setDrawFont(fntOption);
-		con.drawString("(1)Play Game",498,420);
-		con.drawString("(2)View Leaderboard",410,490);
-		con.drawString("(3)Add Quiz",508,560);
-		con.drawString("(4)Quit",560,630);
+		con.drawString("(p)Play Game",510,420);
+		con.drawString("(v)View Leaderboard",410,490);
+		con.drawString("(c)Create Quiz",508,560);
+		con.drawString("(q)Quit",560,630);
 		//Main menu screen options
 		con.repaint();
-		int intKey = con.getKey();
 		
+		int intKey = con.getKey();
+		String strName1;
 		String strChooseSpecificQuiz[];
 			
 		// 1) PLAY GAME
-		if(intKey == '1'){
+		if(intKey == 'p' || intKey == 'P'){
 			con.setDrawColor(Color.WHITE);
 			con.fillRect(0,0,1366,768);
 			con.setTextColor(Color.BLACK); 
 			//clear previous 
-			Font fntGamePlay=con.loadFont("BilonaMedium-2v6W3.ttf",30);
+			Font fntGamePlay=con.loadFont("BilonaMedium-2v6W3.ttf",60);
 			con.setDrawFont(fntGamePlay);
-			con.drawString("Enter Name:",600,300);
+			con.setDrawColor(new Color(0, 0, 0));
+			con.drawString("Enter Name: ",300,300);
 			strName = con.readLine();
-			con.repaint();
-			//con.clear();
+			con.clear();
+			con.drawString((strName),600,300);
 			
 			TextInputFile masterfile = new TextInputFile("quizzes.txt");
 			//input quiz type options onto screen for user to choose - load into 1 dimensional array
 			
 			strChooseSpecificQuiz = new String[100];
 		
+			con.clear();
+			con.repaint();
+			
 			while(masterfile.eof() == false){
 				strChooseSpecificQuiz[intCount] = masterfile.readLine();
 				//the number of quizzes avaliable to choose from
-				con.println((intCount + 1) + ") " + strChooseSpecificQuiz[intCount]);		
+				con.drawString((intCount + 1) + ") " + strChooseSpecificQuiz[intCount],150,500);		
 				intCount++;
 				//+1 -1 is due to index, since it has to start at 0
+				
+				//USE A KEY HERE - NEED TO ADD
 		}
 		
 		int intQuizChoice; 
 		
-		con.println("Choose a quiz by number");
+		con.drawString("Quiz List",200,900);
 		intQuizChoice = con.readInt();
 		
 		con.clear();
 		
+		int intAnswerChecker=0;
+		int intScore;
+			
 		if(intQuizChoice>0 && intQuizChoice < intCount){
 			con.println("(1)Start Quiz");
 			int int1 = con.getKey();
@@ -110,11 +120,12 @@ public class CPTKaelynLee{
 				strAnswer = con.readLine();
 				
 				if(strAnswer.equalsIgnoreCase(strQuiz[intiam][1]) || strAnswer.equalsIgnoreCase(strQuiz[intiam][2]) || strAnswer.equalsIgnoreCase(strQuiz[intiam][3])){
-					con.println("TEST: correct");
-					//remove?
+					con.println("Correct");
+					intAnswerChecker++;
+					con.println(intAnswerChecker+"/10");
 					
 				}else{
-					con.println("TEST: wrong");
+					con.println("Wrong");
 					//remove?
 					
 				}
@@ -127,8 +138,11 @@ public class CPTKaelynLee{
 		//new screen(MAKE SURE) - NOT DONE YET|
 				con.println(strName);
 				con.println(strChooseSpecificQuiz[intQuizChoice - 1]);
+				con.println(intAnswerChecker+"/10");
+				intScore = intAnswerChecker/10*100;
+				con.println(intScore);
 	
-		}else if(intKey == '2'){
+		}else if(intKey == 'v'){
 			con.println("Leaderboard");
 			
 			//Output user score, quiz name, and user name to the file, arrange based on highest to lowest
@@ -140,7 +154,7 @@ public class CPTKaelynLee{
 		//	endofQuiz.println();
 			
 		
-		}else if(intKey == '3'){
+		}else if(intKey == 'c' || intKey == 'C'){
 			
 			String strNewGameName;
 			String strNewQuestion;
@@ -164,7 +178,7 @@ public class CPTKaelynLee{
 				con.println("Answer 3: ");
 				strNewAnswer3 = con.readLine();
 				
-				TextOutputFile newquiz = new TextOutputFile("strNewGameName.txt");
+				TextOutputFile newquiz = new TextOutputFile(strNewGameName +".txt");
 				//MAKE SURE THE GAME NAME and then .txt (CURRENTLY WRONG)
 				newquiz.println(strNewQuestion);
 				newquiz.println(strNewAnswer1);
@@ -175,7 +189,7 @@ public class CPTKaelynLee{
 	}
 		
 		
-		else if(intKey == '4'){
+		else if(intKey == 'q' || intKey == 'Q'){
 			System.exit(0);
 			
 			//mouse input - clik quit to go back 
